21cb9107a3bc4dca74b7dfee0bd97cdf2511fa1d
Cameron Chandler <webstormcam@gmail.com>
2023-12-01 23:45:35 -0500

    Will work on the main body copy now

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import Photo from './photo.jsx'
+import Body from './body.jsx'
 function App() {
 
   return (
     <>
      <div className='container'>
       <Photo/>
+      <Body/>
      </div>
       
     </>
---
+++ src/body.jsx
@@ -0,0 +1,9 @@
+function Body(){
+    return (
+       <main>
+        
+       </main>
+    )
+}
+
+export default Body
