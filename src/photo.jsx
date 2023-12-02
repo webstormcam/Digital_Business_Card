a8bcc57d96c0e5858a5ce92d0b1a6ec153dddd34
Cameron Chandler <webstormcam@gmail.com>
2023-12-01 23:41:53 -0500

    Deleted some boiler plate and added image
    I deleted some of the vite boiler plate stuff and placed in my asset and code

---
+++ src/photo.jsx
@@ -0,0 +1,10 @@
+import reactLogo from './assets/super-mario-bros_image.webp'
+
+
+function Photo(){
+    return (
+        <img className='photo' src={reactLogo}></img>
+    )
+}
+
+export default Photo
