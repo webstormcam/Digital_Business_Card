639541392471d0e8a28dfd8971aef6032c7dc33f
Cameron Chandler <webstormcam@gmail.com>
2023-12-02 09:19:10 -0500

    Styled more of the main area
    Just gotta put the icons in the buttons

--- src/body.jsx
+++ src/body.jsx
@@ -1,7 +1,21 @@
 function Body(){
     return (
-       <main>
-        
+       <main className="main-area">
+            <div className="top-words">
+                <h1>Mario Mario</h1>
+                <h2>Executive Plumber</h2>
+                <p><a target="_blank" href="https://www.nintendo.com/">www.nintendo.com</a></p>
+            </div>
+            <div className="buttons">
+                <a className="button email" href="#" target="_blank">Email</a>
+                <a className="button linkedin" href="https://www.linkedin.com/in/cameron-chandler-222947197/" target="_blank">LinkedIn</a>
+            </div>
+            <div className="text-place">
+                <h3>About</h3>
+                <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+                <h3>Interests</h3>
+                <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            </div>
        </main>
     )
 }
