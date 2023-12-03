8d5baea5d9bb9caf0ab24b0396686dcd073e55d4
Cameron Chandler <webstormcam@gmail.com>
2023-12-03 12:29:02 -0500

    Finished building out the footer section

--- src/footer.jsx
+++ src/footer.jsx
@@ -8,7 +8,14 @@ import twitter from "./assets/Twitter_Icon.png"
 
 function Footer(){
     return (
-        <footer></footer>
+        <footer>
+            <div className="footer_container">
+                <a href="https://twitter.com/NintendoAmerica?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank"><img src={twitter} alt="Twitter icon" /></a>
+               <a href="https://www.facebook.com/nintendoofcanada/" target="_blank"> <img src={faceBook} alt="Facebook Icon" /></a>
+                <a href="https://www.instagram.com/nintendoamerica/?hl=en" target="_blank"><img src={instaGram} alt="Instagram Icon" /></a>
+               <a href="https://github.com/Nintendo" target="_blank"><img src={gitHub} alt="Github Icon" /></a>
+            </div>
+        </footer>
     )
 }
 
