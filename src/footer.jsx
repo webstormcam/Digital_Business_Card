2776f6d2dd995f5b7207b8d43d52006a45c5f87c
Cameron Chandler <webstormcam@gmail.com>
2023-12-03 12:14:23 -0500

    Update footer.jsx

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,3 +1,11 @@
+
+import faceBook from "./assets/Facebook_Icon.png"
+import gitHub from "./assets/GitHub_Icon.png"
+import instaGram from "./assets/Instagram_Icon.png"
+import twitter from "./assets/Twitter_Icon.png"
+
+
+
 function Footer(){
     return (
         <footer></footer>
